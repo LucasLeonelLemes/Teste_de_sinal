363a8ed1744a7a2d5645a5cec775e5c307167b1a
mafiozo1518 <112278104+mafiozo1518@users.noreply.github.com>
2023-08-12 14:29:33 -0300

    Add files via upload

---
+++ app_sinal.js
@@ -0,0 +1,50 @@
+
+const sinalInicial = 3.04
+
+const splitter = 5 / 95
+
+
+
+if (splitter === 5 / 95) {
+    const spliterUm = sinalInicial - 14.60
+    const spliterDois = sinalInicial - 0.50
+    console.log(`a saida do 5% sera: ${spliterUm}`)
+    console.log(`a saida do 95% sera: ${spliterDois}`)
+} else if (splitter === 10 / 90) {
+    const spliterUm = sinalInicial - 11.00
+    const spliterDois = sinalInicial - 0.70
+    console.log(`a saida do 10% sera: ${spliterUm}`)
+    console.log(`a saida do 90% sera: ${spliterDois}`)
+} else if (splitter === 15 / 85) {
+    const spliterUm = sinalInicial - 9.60
+    const spliterDois = sinalInicial - 1.00
+    console.log(`a saida do 15% sera: ${spliterUm}`)
+    console.log(`a saida do 85% sera: ${spliterDois}`)
+} else if (splitter === 20 / 80){
+    const spliterUm = sinalInicial - 7.90
+    const spliterDois = sinalInicial - 1.40
+    console.log(`a saida do 20% sera: ${spliterUm}`)
+    console.log(`a saida do 80% sera: ${spliterDois}`)
+} else if (splitter === 25 / 75){
+    const spliterUm = sinalInicial - 6.95
+    const spliterDois = sinalInicial - 1.70
+    console.log(`a saida do 25% sera: ${spliterUm}`)
+    console.log(`a saida do 75% sera: ${spliterDois}`)
+} else if (splitter === 30 / 70){
+    const spliterUm = sinalInicial - 6.00
+    const spliterDois = sinalInicial - 1.90
+    console.log(`a saida do 30% sera: ${spliterUm}`)
+    console.log(`a saida do 70% sera: ${spliterDois}`)
+} else if (splitter === 40 / 60){
+    const spliterUm = sinalInicial - 4.70
+    const spliterDois = sinalInicial - 2.70
+    console.log(`a saida do 40% sera: ${spliterUm}`)
+    console.log(`a saida do 60% sera: ${spliterDois}`)
+} else if (splitter === 50 / 50){
+    const spliterUm = sinalInicial - 3.50
+    const spliterDois = sinalInicial - 3.50
+    console.log(`a saida do 50% sera: ${spliterUm}`)
+    console.log(`a saida do 50% sera: ${spliterDois}`)
+}
+
+
